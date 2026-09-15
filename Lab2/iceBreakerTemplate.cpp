47b808643e0d2434569512b367f127b9ead2bd22
NatePaz23 <nxpaz@cpp.edu>
2026-09-14 20:22:33 -0700

    Update iceBreakerTemplate.cpp

--- Lab2/iceBreakerTemplate.cpp
+++ Lab2/iceBreakerTemplate.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 #include<vector>
 #include<cstdlib>
 #include<ctime>
-
 /**
  * TO DO: 
  * Add <vector>, <cstdlib>, and <ctime>​
@@ -24,10 +24,11 @@ using namespace std;
 //------------------------PROTOTYPE-------------------------------------------
 void promptFile(vector<string> &); 
 void printVec(vector<string>);
-int ranGen(int);
 void readFile(string filename, vector<string> & vec);
-void writeFile(string filename, const vector<string> & v0, const vector<string> & v1)
-
+void writeFile(string filename, vector<string> v0, vector<string> v1);
+void promptFile(vector<string> & v);
+void printVec(vector<string> v);
+int ranGen();
 /**
  * @brief randomly returns a number from 0 to 5.
  * - It is hardcoded to be from 0 to 5.
@@ -41,8 +42,9 @@ void writeFile(string filename, const vector<string> & v0, const vector<string> 
  * 
  * @return int: index of question
  */
-int ranGen(){
-    int randomNumber = rand() % 6;  // 0 through 5
+int ranGen(int size)
+{
+    int randomNumber = rand() % size;  // 0 through qBank.size() - 1
     return randomNumber;
 }
 
@@ -95,7 +97,7 @@ void readFile(string filename, vector<string> & vec) {
  * -  pass by value (e.g. vector<string> v0),
  * -  pass by const reference (e.g. const vector<string> & v0),
  */
-void writeFile(string filename, const vector<string> & v0, const vector<string> & v1){
+void writeFile(string filename, vector<string> v0, vector<string> v1){
 
     ofstream outputFile(filename);
      if (!outputFile) {
