f54fcc003ab7ae33b4fb6b56afb8b034096a39b1
NatePaz23 <nxpaz@cpp.edu>
2026-09-22 09:50:04 -0700

    Update arraydemo.cpp

--- Lab1/arraydemo.cpp
+++ Lab1/arraydemo.cpp
@@ -3,19 +3,12 @@
 using namespace std;
 
 
-void printMemArr(const int arr[],int size) {
-    printf("Array - Each in is worth %lu bytes\n", sizeof(arr[0]));
-for (int i = 0 ; i<size; i++){
-    printf("Value: %i at Memory Location: %p\n", arr[i], arr+1);
-}
-}
+void printMemArr(const int arr[],int size); 
 
 
-void incArrBy10 (int arr[], int size) {
-    for (int i = 0 ; i < size; i++){
-arr[i]+=10;
-    }
-}
+
+void incArrBy10 (int arr[], int size) ;
+
 
 int main(){
     const int SIZE=5;
@@ -33,6 +26,19 @@ int main(){
     printf("After-------\n");
     printMemArr(arr, SIZE);
 
-    return 0;
     
+    
+}
+
+void incArrBy10 (int arr[], int size) {
+    for (int i = 0 ; i < size; i++){
+arr[i]+=10;
+    }
+}
+
+void printMemArr(const int arr[],int size) {
+    printf("Array - Each in is worth %lu bytes\n", sizeof(arr[0]));
+for (int i = 0 ; i<size; i++){
+    printf("Value: %i at Memory Location: %p\n", arr[i], arr+1);
+}
 }
