16252c977b54f9c7c976bf427b254a0f845aa2e5
NatePaz23 <nxpaz@cpp.edu>
2026-09-24 11:04:28 -0700

    Updated contructor variables

--- Demos/Person/Person.cpp
+++ Demos/Person/Person.cpp
@@ -10,5 +10,8 @@ Person::Person(){
 
 Person::Person::Person(string name, int age, string occupation, bool lives_in_IE){
 
-    
+  this->name = name;
+  this->age = age;
+  this->occupation = occupation;
+  this->lives_in_IE = lives_in_IE;
 }
