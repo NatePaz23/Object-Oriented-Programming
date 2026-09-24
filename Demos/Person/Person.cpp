3620972c7e954d7ade5508b9142e6c6b55e17b23
NatePaz23 <nxpaz@cpp.edu>
2026-09-24 11:06:21 -0700

    Update name and age

--- Demos/Person/Person.cpp
+++ Demos/Person/Person.cpp
@@ -15,3 +15,13 @@ Person::Person::Person(string name, int age, string occupation, bool lives_in_IE
   this->occupation = occupation;
   this->lives_in_IE = lives_in_IE;
 }
+
+// updates name to new_name 
+void Person::updateName(string new_name){
+    name= new_name;
+}
+
+// updates age to new_age 
+void Person::updateAge(int new_age){
+    age = new_age;
+}
