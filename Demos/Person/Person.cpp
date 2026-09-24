98e843ae76d18fce8f1a821fafec0a65a87fb1e0
NatePaz23 <nxpaz@cpp.edu>
2026-09-24 11:02:56 -0700

    Defualt Constructor

---
+++ Demos/Person/Person.cpp
@@ -0,0 +1,14 @@
+//Person.cpp
+#include "Person.h"
+
+Person::Person(){
+    name = "" ;
+    age = 1;
+    occupation = "";
+    lives_in_IE = false;
+}
+
+Person::Person::Person(string name, int age, string occupation, bool lives_in_IE){
+
+    
+}
